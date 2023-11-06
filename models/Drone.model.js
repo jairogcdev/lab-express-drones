9ad3d769ccbb86e2632d16d6902a69064ceffa9b
jairohrdev <j.gcarballa.dev@gmail.com>
2023-11-06 17:09:34 +0100

    Iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const { Schema, model } = require("mongoose");
+
+const dronesSchema = new Schema({
+  name: String,
+  propellers: Number,
+  maxSpeed: Number,
+});
+
+const Drones = model("Drones", dronesSchema);
+
+module.exports = Drones;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,19 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drones = require("../models/Drone.model");
+const allDrones = require("../seeds/drones");
+const connection = async (next) => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/lab-express-drones");
+    console.log("Estas conectado a la BD");
+    await Drones.create(allDrones);
+    console.log("Creados : " + allDrones.length + " dron/es");
+    console.log("Todos los drones han sido creados");
+    await mongoose.disconnect();
+    console.log("Conexión a BD cerrada");
+  } catch (error) {
+    next(error);
+  }
+};
+
+connection();
